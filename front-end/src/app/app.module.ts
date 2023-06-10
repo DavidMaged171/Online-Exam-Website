09964a399e9b46ff1c2897c46aa7863dd3172e0c
David Maged <davidmaged171@gmail.com>
2023-06-10 11:25:37 +0300

    Added Login & Register Components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -11,13 +11,18 @@ import { HeaderComponent } from './shared/header/header.component';
 import { Route, RouterModule } from '@angular/router';
 import { EditQuestionComponent } from './question-components/edit-question/edit-question.component';
 import { ViewQuestionComponent } from './question-components/view-question/view-question.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routs:Route[]=[
   {path:"question/create",component:AddQuestionComponent},
   {path:"question/update/:questionId",component:EditQuestionComponent},
   {path:"question/read/:questionId",component:ViewQuestionComponent},
   {path:"question/delete/:questionId",component:QuestionListComponent},
-  {path:'',component:QuestionListComponent}
+  {path:'questionList',component:QuestionListComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'',component:LoginComponent}
 ]
 
 @NgModule({
@@ -29,6 +34,8 @@ const routs:Route[]=[
     HeaderComponent,
     EditQuestionComponent,
     ViewQuestionComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
